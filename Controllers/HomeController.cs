a10aa5516a956a4103b0d5bf4b73b6ff8625be2e
Cumboy228 <nazaryagotin@gmail.com>
2024-06-03 00:06:43 +0300

    Updated HomePage design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Tourism.Models;
 
 namespace Tourism.Controllers;
@@ -7,14 +8,17 @@ namespace Tourism.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly TourismDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, TourismDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        ViewData["Photos"] = _context.Photos.ToList();
         return View();
     }
 
