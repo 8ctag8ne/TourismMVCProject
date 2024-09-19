4c23cdb44ecc0424d65cdfd336e79da03a10688a
Cumboy228 <nazaryagotin@gmail.com>
2024-09-19 13:51:17 +0300

    адекватний вмiст

--- obj/Debug/net8.0/Tourism.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tourism.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tourism")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a10aa5516a956a4103b0d5bf4b73b6ff8625be2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e44b8eae70c6ae2380c2329a82de2e3603a69e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tourism")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tourism")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
